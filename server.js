5e63837d5bc8ce1eba4f27c6f1c32092f2461076
Junior Fortunato <79145717+fcofortunato@users.noreply.github.com>
2021-03-08 23:03:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/pokemon'));
+app.use(express.static('./dist/gerenciador-de-tarefas'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/pokemon' });
+  res.sendFile('index.html', { root: 'dist/gerenciador-de-tarefas' });
 });
 
 app.listen(process.env.PORT || 8080);
